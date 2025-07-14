98f944e07baac5e83cfbe5940fad6bb7beb3d9e4
Vlad Keliy <vekkele@gmail.com>
2025-07-14 20:26:00 +0300

    🔧 Adjust KeyRepeat

--- macos-settings.sh
+++ macos-settings.sh
@@ -34,7 +34,7 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 3
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Sets the trackpad speed to 3
